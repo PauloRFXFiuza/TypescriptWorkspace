1e0842c22afb9ba2414aa55e6863836eedeec04c
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-30 10:08:22 -0300

    v2.2

--- ProjectAdvancesTypes/advancedTypes.js
+++ ProjectAdvancesTypes/advancedTypes.js
@@ -1,1 +1,13 @@
 "use strict";
+/* ADVANCED OR SPECIAL TYPES
+TIPOS AVANÇADOS OU ESPECIAIS*/
+/* union type - when a value can be more than one type
+quando um valor pode ser mais do que um único tipo */
+let myVariable;
+myVariable = "Hello";
+myVariable = 42;
+//myVariable = true; //error
+/* any - a type that disables type checking and effectively allows all types to be used.
+(qualquer) - é um tipo que desabilita a verificação de tipos e permite efetivamente que todos os tipos sejam usados. */
+let teste = 100;
+teste = "text";
