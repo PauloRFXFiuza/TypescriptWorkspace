db97cb584d69efef8d349dd6ee5959a5342d01e4
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-30 08:57:08 -0300

    v1.9

--- BasicProject1/types.js
+++ BasicProject1/types.js
@@ -1,3 +1,11 @@
 "use strict";
 /* Basic, simple, primitive data types
-Tipos básicos, simples, primitivos de dados */ 
+Tipos básicos, simples, primitivos de dados */
+/* string -text values- valores de texto */
+let my_name = "Paulo Fiuza";
+/* number -integer and float numbers- números inteiros e ponto flutuante */
+let age = 33;
+/* boolean- true and false values- valores verdadeiro e falso */
+let activate = true;
+/* undefined  - indefinido */
+let und_value = undefined;
--- BasicProject1/types.ts
+++ BasicProject1/types.ts
@@ -1,2 +1,14 @@
 /* Basic, simple, primitive data types
 Tipos básicos, simples, primitivos de dados */
+
+/* string -text values- valores de texto */
+let my_name: string = "Paulo Fiuza";
+
+/* number -integer and float numbers- números inteiros e ponto flutuante */
+let age: number = 33;
+
+/* boolean- true and false values- valores verdadeiro e falso */
+let activate: boolean = true;
+
+/* undefined  - indefinido */
+let und_value: undefined = undefined;
