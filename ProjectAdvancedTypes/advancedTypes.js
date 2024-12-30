0ade742a8e0b9e400c146e5aa20dcca397ba9e9a
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-30 10:12:11 -0300

    v2.3

--- ProjectAdvancedTypes/advancedTypes.js
+++ ProjectAdvancedTypes/advancedTypes.js
@@ -1,16 +1,13 @@
-/* ADVANCED OR SPECIAL TYPES 
+"use strict";
+/* ADVANCED OR SPECIAL TYPES
 TIPOS AVANÇADOS OU ESPECIAIS*/
-
 /* union type - when a value can be more than one type
 quando um valor pode ser mais do que um único tipo */
-
-let myVariable: string | number;
+let myVariable;
 myVariable = "Hello";
 myVariable = 42;
-//myVariable = true; //error
-
+//myVariable = true; //TS error: Type 'boolean' is not assignable to type 'string | number'.ts(2322)
 /* any - a type that disables type checking and effectively allows all types to be used.
 (qualquer) - é um tipo que desabilita a verificação de tipos e permite efetivamente que todos os tipos sejam usados. */
-
-let teste: any = 100;
-teste = "text";
+let test = 100;
+test = "text";
--- ProjectAdvancedTypes/advancedTypes.ts
+++ ProjectAdvancedTypes/advancedTypes.ts
@@ -1,13 +1,16 @@
-"use strict";
-/* ADVANCED OR SPECIAL TYPES
+/* ADVANCED OR SPECIAL TYPES 
 TIPOS AVANÇADOS OU ESPECIAIS*/
+
 /* union type - when a value can be more than one type
 quando um valor pode ser mais do que um único tipo */
-let myVariable;
+
+let myVariable: string | number;
 myVariable = "Hello";
 myVariable = 42;
-//myVariable = true; //error
+//myVariable = true; //TS error: Type 'boolean' is not assignable to type 'string | number'.ts(2322)
+
 /* any - a type that disables type checking and effectively allows all types to be used.
 (qualquer) - é um tipo que desabilita a verificação de tipos e permite efetivamente que todos os tipos sejam usados. */
-let teste = 100;
-teste = "text";
+
+let test: any = 100;
+test = "text";
