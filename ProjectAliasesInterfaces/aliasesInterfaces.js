b40e5e8c9176b16b6334544c09692111e12a6da1
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-01-06 08:10:58 -0300

    v3.1

--- ProjectAliasesInterfaces/aliasesInterfaces.js
+++ ProjectAliasesInterfaces/aliasesInterfaces.js
@@ -1,1 +1,14 @@
 "use strict";
+/* Aliases And Interfaces
+Basically, it's about creating your own custom types.
+Type aliases allow you to define types with a custom name
+(an alias) for primitive or complex types.
+Interfaces are the same as aliases, except they only apply to object types.
+
+Aliases E Interfaces
+Basicamente é criar os seus próprios tipos personalizados
+Os aliases de tipo permitem definir tipos com um nome personalizado
+(um alias) de tipos primitivos ou complexos.
+As interfaces são a mesma coisa que os aliases, exceto que só se aplicam ao tipo objeto.
+*/
+let a = 2;
